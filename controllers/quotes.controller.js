31f99e0c78146d6928d35f6361d6a26c18b6f0c7
Sudhir Kenguva <sudhirkenguva@Sudhirs-MacBook-Air.local>
2020-11-12 09:52:53 +0530

    udpate and delete API

--- controllers/quotes.controller.js
+++ controllers/quotes.controller.js
@@ -1,24 +1,56 @@
 let QuotesSchema = require("../models/quotes.mongoSchema")
 async function getAllQuotes(req, res, next) {
-
-  let quotes = await QuotesSchema.find();
+  let quotes = await QuotesSchema.find({}, {});
   res.status(400).send({msg: "succss", data: quotes})
 };
 
-function getRandomQuote(req, res, next) {
-var quotes = require("../data/quotes.json");
-let min = 0;
-let max = quotes.length - 1;
-let ind = Math.random() * (max - min) + min;
-ind = Math.ceil(ind);
-let quote = quotes[ind]
-res.status(400).send({msg: "succss", data: quote})
+async function getRandomQuote(req, res, next) {
+  let min = 0;
+  let quotes = await QuotesSchema.find();
+  let max = quotes.length - 1;
+  let ind = Math.random() * (max - min) + min;
+  ind = Math.ceil(ind);
+  let quote = quotes[ind]
+  res.status(400).send({ msg: "success", data: quote })
+};
+
+async function updateQuote(req, res, next) {
+  try{
+    let id = req.params.id; 
+    let payload = req.body; 
+    if(!id || Object.keys(payload).length == 0) {
+      res.status(400).send({ msg: "Bad request"});
+    }
+    await QuotesSchema.update({_id: id}, {$set: payload});
+    res.status(200).send({ msg: "success"});
+  } catch(ex) {
+    console.log(ex);
+    res.status(500).send({ msg: "Internal Server Error"});
+  }
+
+};
+
+async function deleteQuote(req, res, next) {
+  try{
+    let id = req.params.id; 
+    if(!id) {
+      res.status(400).send({ msg: "Bad request"});
+    }
+    await QuotesSchema.deleteOne({_id: id})
+    res.status(200).send({ msg: "success"});
+  } catch(ex) {
+    console.log(ex);
+    res.status(500).send({ msg: "Internal Server Error"});
+  }
+
 };
 
 
 let ctrlMethods = {
   getAllQuotes: getAllQuotes,
-  getRandomQuote: getRandomQuote
+  getRandomQuote: getRandomQuote,
+  updateQuote: updateQuote,
+  deleteQuote: deleteQuote
 }
 
 module.exports = ctrlMethods;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 const quotesCtrl = require('../controllers/quotes.controller')
 
 /* GET home page. */
+
+//get all quotes
 router.get('/api/quote', quotesCtrl.getAllQuotes);
+
+// get random quote
 router.get('/api/random-quote', quotesCtrl.getRandomQuote);
+
+// update single quote
+router.put('/api/quote/:id', quotesCtrl.updateQuote);
+
+// delete quote
+router.delete('/api/quote/:id', quotesCtrl.deleteQuote);
 
 module.exports = router;
